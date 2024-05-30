e976dfd509bb23b3bccccee4ded274e2dd273c20
freshposh-code <atomisefarouk919@gmail.com>
2024-05-30 14:18:59 +0100

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom'
 import { Header } from './components'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
       <Header />
-      {/* <Outlet /> */}
+      <div>
+        <Outlet />
+      </div>
       <Home />
+      <Footer />
     </>
   )
 }
