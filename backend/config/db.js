2903e0e0f29dbf7d5f78d068cf2067febdc0318a
freshposh-code <atomisefarouk919@gmail.com>
2024-06-06 21:45:59 +0100

    DB setup

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+
+async function connectDB() {
+    try {
+        await mongoose.connect('mongodb+srv://atomisefarouk919:D01FjZV9N05j3izI@cluster0.jbf0q6a.mongodb.net/MERN E-COMMERCE?retryWrites=true&w=majority&appName=Cluster0')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = connectDB
