b8cd23f7d0c646f75a239e829b9a9f8180f81dc4
freshposh-code <atomisefarouk919@gmail.com>
2024-06-08 20:56:43 +0100

    backend express setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function connectDB() {
     try {
-        await mongoose.connect('mongodb+srv://atomisefarouk919:D01FjZV9N05j3izI@cluster0.jbf0q6a.mongodb.net/MERN E-COMMERCE?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect('mongodb+srv://atomisefarouk919:D01FjZV9N05j3izI@cluster0.jbf0q6a.mongodb.net/MERNE-commerce?retryWrites=true&w=majority&appName=Cluster0')
     } catch (err) {
         console.log(err)
     }
---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const router = require('./routes')
+const cookieParser = require('cookie-parser')
+
+require('dotenv').config()
+
+
+const app = express()
+app.use(cors(
+    {
+        origin: 'http://localhost:5173', // Specify the frontend URL
+        credentials: true,               // Allow credentials (cookies, HTTP auth)
+    }
+))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api', router)
+
+const PORT = 8080 || process.env.PORT
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("connected to DB")
+        console.log("Server is running " + PORT)
+    })
+})
