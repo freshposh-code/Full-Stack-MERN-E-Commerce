43121ff5f493a74a2d38dc24143045e553720153
freshposh-code <atomisefarouk919@gmail.com>
2024-06-10 17:20:14 +0100

    pass backend methods

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,10 +4,13 @@ const router = express.Router()
 
 const userSignUpController = require("../controller/user/userSignUp")
 const userSignInController = require("../controller/user/userSignIn")
+const userDetailsController = require("../controller/user/userDetails")
+const authToken = require('../middleware/authToken')
 
 
 
 router.post("/signup", userSignUpController)
 router.post("/signin", userSignInController)
+router.get("/user-details", authToken, userDetailsController)
 
 module.exports = router
