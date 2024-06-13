ebf5e1a63902d2bafced4f5573353f7be50d052d
freshposh-code <atomisefarouk919@gmail.com>
2024-06-13 01:26:57 +0100

    authenticate user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import loginIcons from '../assest/signin.gif'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaEye, FaEyeSlash } from 'react-icons/fa6'
-// import SummaryApi from '../common'
 import { toast } from 'react-toastify'
 import SummaryApi from '../common'
+import Context from '../context'
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
@@ -13,6 +13,8 @@ const Login = () => {
         email: "",
         password: ""
     });
+
+    const { fetchUserDetails } = useContext(Context)
 
     const handleOnChange = (e) => {
         const { name, value } = e.target
@@ -44,7 +46,7 @@ const Login = () => {
         if (dataApi.success) {
             toast.success(dataApi.message)
             navigate('/')
-            // fetchUserDetails();
+            fetchUserDetails();
             // fetchUserAddToCart();
         }
 
