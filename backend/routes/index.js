f9048e2c74d533b4469ab695789af03cc824ab4d
freshposh-code <atomisefarouk919@gmail.com>
2024-06-13 01:24:06 +0100

    update backend route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,11 +6,13 @@ const userSignUpController = require("../controller/user/userSignUp")
 const userSignInController = require("../controller/user/userSignIn")
 const userDetailsController = require("../controller/user/userDetails")
 const authToken = require('../middleware/authToken')
+const userLogout = require('../controller/user/userLogout')
 
 
 
 router.post("/signup", userSignUpController)
 router.post("/signin", userSignInController)
 router.get("/user-details", authToken, userDetailsController)
+router.get('/userLogout', userLogout)
 
 module.exports = router
