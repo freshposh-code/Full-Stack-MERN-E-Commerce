9f99a4f542f87d9068991aca1b2d56d5ac331835
freshposh-code <atomisefarouk919@gmail.com>
2024-07-02 23:26:09 +0100

    set banner data

---
+++ src/helpers/Data/index.js
@@ -0,0 +1,20 @@
+export const swiperImages = [
+    {
+        image: `https://mindstacktechnologies.com/wordpress/wp-content/uploads/2018/01/ecommerce-banner.jpg`
+    },
+    {
+        image: `https://static.vecteezy.com/system/resources/previews/004/707/493/non_2x/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-vector.jpg`
+    },
+    {
+        image: `https://www.internetcreation.net/wp-content/uploads/2015/04/banner-e-commerce1.png`
+    },
+    {
+        image: `https://www.jdmedia.co.za/images/carousel/Ecommerce-Banner-1920.jpg`
+    },
+    {
+        image: `https://static.vecteezy.com/system/resources/thumbnails/011/871/814/small/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-vector.jpg`
+    },
+    {
+        image: `https://linkusuponline.com/wp-content/uploads/2019/08/e-commerce-banner-1024x341.jpg`
+    },
+]
