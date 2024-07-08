7681cbccd63c6cdedea6ab2a03ef12317525ee7e
freshposh-code <atomisefarouk919@gmail.com>
2024-07-08 22:21:28 +0100

    handle usersCart for App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Footer, Header } from './components'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SummaryApi from './common';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Context from './context';
 import { useDispatch } from 'react-redux';
 import { setUserDetails } from './store/userSlice';
@@ -12,6 +12,7 @@ import Loader from './components/Loader';
 
 const App = () => {
   const dispatch = useDispatch();
+  const [cartProductCount, setCartProductCount] = useState(0)
 
   const fetchUserDetails = async () => {
     const dataResponse = await fetch(SummaryApi.current_user.url, {
@@ -30,20 +31,33 @@ const App = () => {
     // setCartProductCount(dataApi?.data?.count)
   }
 
+  const fetchUserAddToCart = async () => {
+    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url, {
+      method: SummaryApi.addToCartProductCount.method,
+      credentials: 'include'
+    })
+
+    const dataApi = await dataResponse.json()
+
+    console.log('add to cart product', dataApi)
+
+    setCartProductCount(dataApi?.data?.count)
+  }
+
   useEffect(() => {
     /**user Details */
     fetchUserDetails()
     /**user Details cart product */
-    // fetchUserAddToCart()
+    fetchUserAddToCart()
 
   }, [])
 
   return (
     <>
       <Context.Provider value={{
-        fetchUserDetails
+        fetchUserDetails, fetchUserAddToCart, cartProductCount
       }}>
-        <ToastContainer />
+        <ToastContainer position='top-center' />
         <Header />
         <main className='pt-16'>
           <Outlet />
