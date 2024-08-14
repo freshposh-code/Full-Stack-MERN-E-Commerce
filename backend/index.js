f54b9739388de28f3b159c8175279463f4cf134e
freshposh-code <atomisefarouk919@gmail.com>
2024-08-14 03:30:31 +0100

    update cors in server

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,44 @@
-const express = require('express')
-const cors = require('cors')
-const connectDB = require('./config/db')
-const router = require('./routes')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const router = require('./routes');
+const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
 
+const app = express();
 
+const allowedOrigins = [
+    process.env.REACT_VITE_APP_FRONTEND_URL, // Local development URL
+    'https://fullecommerceposhstore.vercel.app', // Production URL
+];
 
-const app = express()
 app.use(cors({
-    origin: [
-        process.env.REACT_VITE_APP_FRONTEND_URL, // Local development URL
-        'https://fullecommerceposhstore.vercel.app', // Production URL
-    ],
+    origin: function (origin, callback) {
+        // Allow requests with no origin like mobile apps or curl requests
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true,
 }));
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api', router)
+// Handling preflight requests for all routes
+app.options('*', cors());
 
-const PORT = 8080 || process.env.PORT
+app.use('/api', router);
 
+const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log("connected to DB")
-        console.log("Server is running " + PORT)
-    })
-})
+        console.log("Connected to DB");
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
