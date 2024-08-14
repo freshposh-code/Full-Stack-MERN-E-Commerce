ba919bdbae59ad1f7650d80d8ea5eb5c27874e24
freshposh-code <atomisefarouk919@gmail.com>
2024-08-14 02:47:00 +0100

    move: all files/folder to frontend

--- src/assest/index.js
+++
@@ -1,6 +0,0 @@
-import logo from './logo.png'
-import emptybag from './emptybag.png'
-import success from './success.gif'
-import cancel from './cancel.gif'
-
-export { logo, emptybag, success, cancel }
