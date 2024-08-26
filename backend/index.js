b7007019ca2632082570930e3d6c8187632efa45
freshposh-code <atomisefarouk919@gmail.com>
2024-08-27 00:36:22 +0100

    change to CORs

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const router = require('./routes');
-const cookieParser = require('cookie-parser');
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const router = require('./routes')
+const cookieParser = require('cookie-parser')
 
 require('dotenv').config();
 
-const app = express();
-
-// Custom CORS middleware to manually set headers
-app.use((req, res, next) => {
-    const allowedOrigins = [
+const app = express()
+app.use(cors({
+    origin: [
         process.env.REACT_VITE_APP_FRONTEND_URL, // Local development URL
-        'https://poshstore.vercel.app'           // Production URL
-    ];
+        'https://poshstore.vercel.app', // Production URL (no trailing slash)
+    ],
+    credentials: true,
+}));
 
-    const origin = req.headers.origin;
+app.use(express.json())
+app.use(cookieParser())
 
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
-    }
+app.use('/api', router)
 
-    // Intercept OPTIONS method to respond with 200
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-    } else {
-        next();
-    }
-});
+const PORT = 8080 || process.env.PORT
 
-app.use(express.json());
-app.use(cookieParser());
-
-app.use('/api', router);
-
-const PORT = process.env.PORT || 8080;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log("Connected to DB");
-        console.log("Server is running on port " + PORT);
-    });
-});
+        console.log("connected to DB")
+        console.log("Server is running " + PORT)
+    })
+})
