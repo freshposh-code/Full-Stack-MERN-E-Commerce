fad207ac2c63331d29893bbb357c489d938259d4
freshposh-code <atomisefarouk919@gmail.com>
2024-11-05 20:06:03 +0100

    "Refactored CORS configuration and rearranged imports in backend/index.js"

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,27 @@
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
 const connectDB = require('./config/db')
 const router = require('./routes')
-const cookieParser = require('cookie-parser')
 
-require('dotenv').config();
 
 const app = express()
 app.use(cors({
-    origin: [
-        process.env.REACT_VITE_APP_FRONTEND_URL, // Local development URL
-        'https://poshstore.vercel.app', // Production URL (no trailing slash)
-        'https://poshstore1.vercel.app' // Add this line to allow requests from poshstore1.vercel.app
-    ],
-    credentials: true,
-    headers: ['Content-Type', 'Authorization'] // Add this line to specify allowed headers
-}));
-
+    origin : process.env.REACT_VITE_APP_FRONTEND_URL,
+    credentials : true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api', router)
+app.use("/api",router)
 
 const PORT = 8080 || process.env.PORT
 
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("connected to DB")
-        console.log("Server is running " + PORT)
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
     })
 })
