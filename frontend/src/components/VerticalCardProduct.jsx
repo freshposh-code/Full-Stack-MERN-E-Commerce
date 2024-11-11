28acb093e3778cbbbbd4fc9d26cd00fdf76164fb
freshposh-code <atomisefarouk919@gmail.com>
2024-11-11 13:44:34 +0100

    "Updated min-width and max-width values for VerticalCardProduct component"

--- frontend/src/components/VerticalCardProduct.jsx
+++ frontend/src/components/VerticalCardProduct.jsx
@@ -68,14 +68,14 @@ const VerticalCardProduct = ({ category, heading }) => {
     return (
         <div className='container mx-auto px-4 my-6 relative'>
             <h2 className='text-2xl font-semibold py-4'>{heading}</h2>
-            <div className='flex items-center gap-4 md:gap-6 overflow-x-scroll scrollbar-none transition-all'>
+            <div className='flex items-center gap-3 md:gap-6 overflow-x-scroll scrollbar-none transition-all'>
                 {loading ? (
                     loadingList.map((_, index) => (
                         <div
                             key={index}
-                            className='w-full min-w-[210px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'
+                            className='w-full min-w-[200px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'
                         >
-                            <div className='bg-slate-200 h-48 p-4 min-w-[210px] md:min-w-[145px] flex justify-center items-center animate-pulse'></div>
+                            <div className='bg-slate-200 h-48 p-4 min-w-[200px] md:min-w-[145px] flex justify-center items-center animate-pulse'></div>
                             <div className='p-4 grid gap-3'>
                                 <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200'></h2>
                                 <p className='capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200 py-2'></p>
@@ -93,9 +93,9 @@ const VerticalCardProduct = ({ category, heading }) => {
                             <Link
                                 to={"product/" + product?._id}
                                 key={index}
-                                className='w-full min-w-[210px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'
+                                className='w-full min-w-[200px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'
                             >
-                                <div className='bg-slate-200 h-40 p-4 min-w-[210px] md:min-w-[145px] flex justify-center items-center'>
+                                <div className='bg-slate-200 h-40 p-4 min-w-[200px] md:min-w-[145px] flex justify-center items-center'>
                                     <img
                                         src={product.productImage[0]}
                                         className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply'
@@ -122,9 +122,9 @@ const VerticalCardProduct = ({ category, heading }) => {
                         loadingList.map((_, index) => (
                             <div
                                 key={index}
-                                className='w-full min-w-[210px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'
+                                className='w-full min-w-[200px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'
                             >
-                                <div className='bg-slate-200 h-48 p-4 min-w-[210px] md:min-w-[145px] flex justify-center items-center animate-pulse'></div>
+                                <div className='bg-slate-200 h-48 p-4 min-w-[200px] md:min-w-[145px] flex justify-center items-center animate-pulse'></div>
                                 <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200'></h2>
                                     <p className='capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200 py-2'></p>
