cfa6da9dbff204cfe0b2db5506c2d39d8745b3af
freshposh-code <atomisefarouk919@gmail.com>
2025-03-22 11:09:52 +0100

    Refactor AdminProductCard component

--- frontend/src/components/AdminProductCard.jsx
+++ frontend/src/components/AdminProductCard.jsx
@@ -8,15 +8,15 @@ const AdminProductCard = ({ data, fetchdata }) => {
 
     return (
         <div className='bg-white p-4 rounded'>
-            <div className='w-40'>
-                <div className='w-32 h-32 flex justify-center items-center m-auto'>
+            <div className='md:w-40 w-28'>
+                <div className='md:w-32 w-20 h-32 flex justify-center items-center m-auto'>
                     <img src={data?.productImage[0]} className='mx-auto object-cover h-full' />
                 </div>
-                <h1 className='text-ellipsis line-clamp-1'>{data.productName}</h1>
+                <h1 className='text-ellipsis line-clamp-1 md:text-sm text-xs'>{data.productName}</h1>
 
                 <div>
 
-                    <p className='font-semibold'>
+                    <p className='font-semibold md:text-base text-sm'>
                         {
                             displayUSACurrency(data.sellingPrice)
                         }
