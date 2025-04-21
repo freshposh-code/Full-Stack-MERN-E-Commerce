9bff011f9299fd5694b298fd6e436b21f4f8a722
freshposh-code <atomisefarouk919@gmail.com>
2025-04-21 14:14:39 +0100

    `Update sameSite option in tokenOption object from 'none' to 'lax' in userSignInController`

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -31,7 +31,7 @@ async function userSignInController(req, res) {
             const tokenOption = {
                 httpOnly: true,
                 secure: true,
-                sameSite: 'none',
+                sameSite: 'lax',
                 maxAge: 7 * 24 * 60 * 60 * 1000
             };
 
